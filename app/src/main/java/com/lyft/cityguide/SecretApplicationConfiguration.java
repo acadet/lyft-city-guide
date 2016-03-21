c30614b2c56875a6d69c517e15591278ce22e3cb
Adrien Cadet <acadet@live.fr>
2016-03-20 18:19:27 -0700

    F🐯ck git add

--- app/src/main/java/com/lyft/cityguide/SecretApplicationConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.lyft.cityguide;
-
-/**
- * SecretApplicationConfiguration
- * <p>
- */
-public class SecretApplicationConfiguration {
-    public final String GOOGLE_API_KEY = "AIzaSyDpdgMRmOCV3erVRZRCp3obmPsi01JPm18";
-}
