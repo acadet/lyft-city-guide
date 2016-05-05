1b8db1f195e4e19049e2cf2f6b8311a969d956db
Adrien Cadet <acadet@live.fr>
2016-05-05 11:26:22 +0200

    Prepares release

--- domain/src/main/java/com/lyft/cityguide/domain/SearchRangeSetting.java
+++ domain/src/main/java/com/lyft/cityguide/domain/SearchRangeSetting.java
@@ -3,6 +3,7 @@ package com.lyft.cityguide.domain;
 /**
  * SearchRangeSetting
  * <p>
+ * Allows user to customize search range
  */
 public enum SearchRangeSetting {
     ONE_MILE, TWO_MILE, FIVE_MILE;
